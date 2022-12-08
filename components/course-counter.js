ac95648dc5350cdacc82966ea3e916724a58436a
roxypoceros <roxypoceros@gmail.com>
2022-12-08 16:02:08 -0600

    Codigo optimizado en course-counter usando un getter para no repetir tanto codigo

--- components/course-counter.js
+++ components/course-counter.js
@@ -27,6 +27,7 @@ super es el constructor de la clase padre. */
   render() {
     return html`
       <h3>Contador del curso de Everfuture</h3>
+      <h4>Llega a 5 clics o a 0 y se lanzará un mensaje</h4>
 
       <div>
         <h2>Llevas <u>${this.counter}</u> clics!!</h2>
@@ -35,8 +36,8 @@ super es el constructor de la clase padre. */
         <!-- Eventos se definen con @ y el nombre del evento = 
       a un método dentro del mismo componente que realizará 
     la operativa de aumentar el número de clics-->
-        <button @click="${this.add}">+1</button>
         <button @click="${this.substract}">-1</button>
+        <button @click="${this.add}">+1</button>
         <br />
         <br />
         <br />
@@ -51,26 +52,43 @@ super es el constructor de la clase padre. */
     `;
   }
 
-  add() {
-    this.counter++;
-    /* Cuando el contador llegue a 5 se mostrará el componente
-    feedback-element (que será un mensaje de feedback) */
-    if (this.counter === 5) {
-      /* para acceder a elementos que están dentro del  template de
-      un webcomponent uso this. shadowroot (es la raíz del documento
-      del template de mi componente)
- */
-        this.shadowRoot.getElementById("feedback").open("¡¡Llegaste a 5 clics!!")
-    }
-  }
-  substract() {
-    this.counter--;
-    if (this.counter === 0) {
-        this.shadowRoot.getElementById("feedback").open("¡¡Legaste a 0!!")
-    }
-  }
+//   
+//   add() {
+//     this.counter++;
+//     /* Cuando el contador llegue a 5 se mostrará el componente
+//     feedback-element (que será un mensaje de feedback) */
+//     if (this.counter === 5) {
+//       /* para acceder a elementos que están dentro del  template de
+//       un webcomponent uso this. shadowroot (es la raíz del documento
+//       del template de mi componente)
+//  */
+//         this.shadowRoot.getElementById("feedback").open("¡¡Llegaste a 5 clics!!")
+//     }
+//   }
+//   substract() {
+//     this.counter--;
+//     if (this.counter === 0) {
+//         this.shadowRoot.getElementById("feedback").open("¡¡Legaste a 0!!")
+//     }
+//   }
+/* Código optimizado con un getter, cuando lo llamen voy a hacer un return 
+de la linea que se repite en add() y substract() */
 
+get feedback(){
+  return this.shadowRoot.getElementById("feedback");
+}
 
+add() {
+  this.counter++;
+  if (this.counter === 5) {
+  /* A this.feedback le pido hacer el open  */
+      this.feedback.open("¡¡Llegaste a 5 clics!!")
+  }}
+substract() {
+  this.counter--;
+  if (this.counter === 0) {
+      this.feedback.open("¡¡Legaste a 0!!")
+  }}
 
 
   
